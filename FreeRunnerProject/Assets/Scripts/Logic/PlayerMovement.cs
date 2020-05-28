90afd4cfd1c6b8400b1f452d64b3e9c7ce508dcd
Gino <ginovandegraaf@hotmail.com>
2020-05-28 16:57:54 +0200

    Unity player movement

---
+++ FreeRunnerProject/Assets/Scripts/Logic/PlayerMovement.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float playerSpeed = 0;
+    [SerializeField]
+    Transform playerTransform;
+    [SerializeField]
+    Rigidbody rig;
+    [SerializeField]
+    BoxCollider boxCollider;
+
+    private float speed=0;
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        JumpMovement();
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    void JumpMovement()
+    {
+
+        SetJumpKey(KeyCode.Space, 10);
+    }
+    void Movement()
+    {
+        speed = Time.deltaTime * playerSpeed;
+        SetInput(KeyCode.LeftArrow, Vector3.left, -1);
+        SetInput(KeyCode.RightArrow, Vector3.right, 1);
+        SetInput(KeyCode.UpArrow, Vector3.forward, 1);
+        SetInput(KeyCode.DownArrow, Vector3.back, -1);
+        
+        
+    }
+
+    bool Isgrounded()
+    {
+        float extraHeight = .2f;
+
+        if (Physics.Raycast(boxCollider.bounds.center, Vector3.down, boxCollider.bounds.extents.y + extraHeight)==false)
+        {
+            Debug.DrawRay(boxCollider.bounds.center, Vector3.down * (boxCollider.bounds.extents.y + extraHeight),Color.red);
+            return false;
+        }
+        else
+        {
+            Debug.DrawRay(boxCollider.bounds.center, Vector3.down * (boxCollider.bounds.extents.y + extraHeight), Color.green);
+        }
+        return true;
+    }
+
+    void SetInput(KeyCode key, Vector3 SetTranslate,float inputSide)
+    {
+        if (Input.GetKey(key))
+        {
+            rig.MovePosition((playerTransform.position+(SetTranslate*speed)));
+        }
+    }
+
+    void SetJumpKey(KeyCode key,float JumpSpeed)
+    {
+        if(Isgrounded() && Input.GetKey(key))
+        {
+            rig.velocity=Vector2.up * JumpSpeed;
+        }
+    }
+
+ 
+}
