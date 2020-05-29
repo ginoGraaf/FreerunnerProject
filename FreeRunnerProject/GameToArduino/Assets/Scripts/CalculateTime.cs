a7025b90dd2b1507f7698c30e239398307d15715
daantjee546 <50410354+daantjee546@users.noreply.github.com>
2020-05-29 15:49:30 +0200

    score tabel added

--- FreeRunnerProject/GameToArduino/Assets/Scripts/CalculateTime.cs
+++ FreeRunnerProject/GameToArduino/Assets/Scripts/CalculateTime.cs
@@ -7,11 +7,22 @@ public class CalculateTime : MonoBehaviour
 {
     public Text timerLabel;
     public Text scoreLabel;
+    public Text First;
+    public Text Second;
+    public Text Thirth;
+    public Text Fourth;
+    public Text Fifth;
 
     private float time = 0;
     private int score = 1;
     private int bonus = 0;
     private bool start = false;
+
+    int first = 0;
+    int second = 0;
+    int thirth = 0;
+    int fourth = 0;
+    int fifth = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -23,22 +34,60 @@ public class CalculateTime : MonoBehaviour
     void Update()
     {
         if(start)
-        time += Time.deltaTime;
+        {
+            time += Time.deltaTime;
 
-        var minutes = time / 60; //Divide the guiTime by sixty to get the minutes.
-        var seconds = time % 60;//Use the euclidean division for the seconds.
-        var fraction = (time * 100) % 100;
+            var minutes = time / 60; //Divide the guiTime by sixty to get the minutes.
+            var seconds = time % 60;//Use the euclidean division for the seconds.
+            var fraction = (time * 100) % 100;
 
-        score += (int)((minutes + seconds + fraction + bonus) / 10);
+            score += (int)((minutes + seconds + fraction + bonus) / 10);
 
-        //update the label value
-        timerLabel.text = string.Format("{0:00} : {1:00} : {2:000}", minutes, seconds, fraction);
-        scoreLabel.text = score.ToString();
+            //update the label value
+            timerLabel.text = string.Format("{0:00} : {1:00} : {2:000}", minutes, seconds, fraction);
+            scoreLabel.text = score.ToString();
+        }
+        
     }
 
     public void StartButtonPressed()
     {
         start = true;
+    }
+    public void StopButtonPressed()
+    {
+        if(int.Parse(First.text) == 0 || score > int.Parse(First.text))
+        {
+            int oldScore = int.Parse(First.text);
+            First.text = score.ToString();
+            score = oldScore;
+        }
+        if(int.Parse(Second.text) == 0 || score > int.Parse(Second.text))
+        {
+            int oldScore = int.Parse(Second.text);
+            Second.text = score.ToString();
+            score = oldScore;
+        }
+        if (int.Parse(Thirth.text) == 0 || score > int.Parse(Thirth.text))
+        {
+            int oldScore = int.Parse(Thirth.text);
+            Thirth.text = score.ToString();
+            score = oldScore;
+        }
+        if (int.Parse(Fourth.text) == 0 || score > int.Parse(Fourth.text))
+        {
+            int oldScore = int.Parse(Fourth.text);
+            Fourth.text = score.ToString();
+            score = oldScore;
+        }
+        if (int.Parse(Fifth.text) == 0 || score > int.Parse(Fifth.text))
+        {
+            int oldScore = int.Parse(Fifth.text);
+            Fifth.text = score.ToString();
+            score = oldScore;
+        }
+        score = 0;
+        start = false;
     }
 
     public void GoodButtonPressed()
