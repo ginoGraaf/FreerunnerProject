08c7596bc3d80aa4a83b4a994b8b4011ff6abf77
Unknown <416145@student.fontys.nl>
2020-06-05 15:17:42 +0200

    Dangerzone movement

--- FreeRunnerProject/Assets/Scripts/ChaseLight.cs
+++ FreeRunnerProject/Assets/Scripts/ChaseLight.cs
@@ -5,17 +5,32 @@ using UnityEngine;
 public class ChaseLight : MonoBehaviour
 {
     [SerializeField] private GameObject Lamp;
+    [HideInInspector] public int state;
     private Material lampMaterial;
 
-    void Start()
+    void Awake()
     {
         lampMaterial = Lamp.GetComponent<Renderer>().material;
-
-        SetState(Color.yellow);
     }
 
-    public void SetState(Color color)
+    public void SetState(int _state)
     {
-        lampMaterial.color = color;
+        state = _state;
+
+        switch (state)
+        {
+            case 0: // lamp turned off
+                lampMaterial.color = Color.black;
+                break;
+            case 1: // lamp in danger zone
+                lampMaterial.color = Color.red;
+                break;
+            case 2: // lamp NOT in danger zone
+                lampMaterial.color = Color.green;
+                break;
+            default:
+                Debug.Log("unknown error occured");
+                break;
+        }
     }
 }
---
+++ FreeRunnerProject/Assets/Scripts/GameController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public ChaseLight[] ChaseLights;
+    [SerializeField] private int firstDangerPoint = 11;
+    [SerializeField] private int dangerZoneLength = 5;
+    float timeLeft = 2.0f;
+
+    void Start()
+    {
+        SetupDangerZone();
+    }
+
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 0)
+        {
+            MoveDangerZone();
+            Debug.Log("move");
+            timeLeft = 2.0f;
+        }
+    }
+
+    private void SetupDangerZone()
+    {
+        for (int _i = 0; _i < dangerZoneLength; _i++)
+        {
+            int _lightToAdd = firstDangerPoint - _i;
+            if (_lightToAdd < 0) // prevent negative numbers
+            {
+                _lightToAdd += ChaseLights.Length;
+            }
+           ChaseLights[_lightToAdd].SetState(1);
+        }
+    }
+
+    private void MoveDangerZone()
+    {
+        // remove last light from dangerzone
+        int _lightToRemove = firstDangerPoint - (dangerZoneLength - 1);
+        if (_lightToRemove < 0)
+        {
+            _lightToRemove += ChaseLights.Length;
+        }
+        ChaseLights[_lightToRemove].SetState(2);
+
+        // add next light to dangerzone
+        firstDangerPoint++;
+        if (firstDangerPoint > ChaseLights.Length - 1)
+        {
+            firstDangerPoint = 0;
+        }
+        ChaseLights[firstDangerPoint].SetState(1);
+    }
+}
