e57cfe10d6b369a65936fa4095ca6cccfe572b33
Unknown <416145@student.fontys.nl>
2020-06-05 13:48:51 +0200

    Lamps

---
+++ FreeRunnerProject/Assets/Scripts/ChaseLight.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaseLight : MonoBehaviour
+{
+    [SerializeField] private GameObject Lamp;
+    private Material lampMaterial;
+
+    void Start()
+    {
+        lampMaterial = Lamp.GetComponent<Renderer>().material;
+
+        SetState(Color.yellow);
+    }
+
+    public void SetState(Color color)
+    {
+        lampMaterial.color = color;
+    }
+}
