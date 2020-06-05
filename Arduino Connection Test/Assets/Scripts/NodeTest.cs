8684168bf30071cbd79a7bcd560d5ad7922943c7
Pimiboj <pimvliet@live.nl>
2020-06-05 10:42:30 +0200

    Added Arduino Connection

---
+++ Arduino Connection Test/Assets/Scripts/NodeTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeTest : MonoBehaviour
+{
+    private ArduinoConnection ard;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ard = transform.GetComponent<ArduinoConnection>();
+        StartCoroutine(ChangeLight());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator ChangeLight()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            ard.SetLight(Random.Range(0, ard.NumberOfNodes), 'R');
+            ard.SetLight(Random.Range(0, ard.NumberOfNodes), '0');
+        }
+    }
+}
