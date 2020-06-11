fe6d392d314da1c77b618169984ff06c77447be3
Gino <ginovandegraaf@hotmail.com>
2020-06-11 10:08:26 +0200

    walking around updated

---
+++ FreeRunnerProject/Assets/MatDetection.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using UnityEngine;
+
+public class MatDetection : MonoBehaviour
+{
+    MatModel[] mats;
+
+    [ReadOnly]
+   public int lookAhead;
+    [SerializeField]
+    int AmountsOfMats;
+     
+    private void Start()
+    {
+        mats = new MatModel[AmountsOfMats];
+        for (int i = 0; i < AmountsOfMats; i++)
+        {
+            mats[i] = new MatModel { LightsOn = false };
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //als niek klaar is kan ik zijn coroutine gebruiken voor dit.
+    }
+}
