00662d708ca754f02bed7d3937791f9ff18aabfa
Unknown <416145@student.fontys.nl>
2020-06-11 13:46:39 +0200

    Copy master to NiekVisualBrance

---
+++ FreeRunnerProject/Assets/MatDetection.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using UnityEngine;
+
+public class MatDetection : MonoBehaviour
+{
+    
+    int AmountsOfMats;
+     
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //als niek klaar is kan ik zijn coroutine gebruiken voor dit.
+    }
+}
